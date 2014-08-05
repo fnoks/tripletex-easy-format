96dba1398caebd388de4cfa0d8418518fbfe9b96
Pål Jørgensen <pal.jorgensen@amendor.com>
2014-08-05 20:24:46 +0200

    Cleaning

--- tripletex-easy-format.js
+++ tripletex-easy-format.js
@@ -2,19 +2,18 @@ var frame = document.getElementsByName('content')[0];
 
 // Wait until the frame(!) is loaded
 frame.onload = function () {
-
   var doc = frame.contentDocument || frame.contentWindow.document;
   var inputs = doc.getElementsByTagName('input');
   var arr = Array.prototype.slice.call(inputs);
 
-  for(var i=0; i<arr.length; i++) {
+  for (var i=0; i<arr.length; i++) {
     var element = arr[i];
-    if(element.id.indexOf('weeks[') !== -1) {
-      element.addEventListener("blur", function( event ) {
+    if (element.id.indexOf('weeks[') !== -1) {
+      element.addEventListener("blur", function (event) {
         var value = event.target.value;
         var parts = value.split(':');
         
-        if(parts.length === 2) {
+        if (parts.length === 2) {
           var element = event.target;
           element.value = parseInt(parts[0]) + parseFloat(parseInt(parts[1])/60);
           if ("createEvent" in doc) {
